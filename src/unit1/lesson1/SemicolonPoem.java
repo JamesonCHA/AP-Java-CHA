e75f2d25c1eeb1280c34d30426785dcda8b30862
JamesonCHA <78878699+JamesonCHA@users.noreply.github.com>
2021-07-12 14:55:41 -0500

    Add lesson 1

---
+++ src/unit1/lesson1/SemicolonPoem.java
@@ -0,0 +1,11 @@
+package unit1.lesson1;
+
+public class SemicolonPoem {
+
+    public static void main(String[] args) {
+        System.out.println("Syntax errors aren't fun");
+        System.out.println("But what we've probably done");
+        System.out.println("Is forget something at the end of the line");
+        System.out.println("Let's begin; it's semicolon time!");
+    }
+}
