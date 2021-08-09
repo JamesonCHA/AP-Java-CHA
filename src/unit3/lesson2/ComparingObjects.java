226c90b0cdaa5320f8bb13ef1e568d72ac31ec07
JamesonCHA <78878699+JamesonCHA@users.noreply.github.com>
2021-08-09 16:07:00 -0500

    Add unit 3, lesson 2 (3.6/3.7: Boolean Expressions & Comparing Objects)

--- src/unit3/lesson2/ComparingObjects.java
+++ src/unit3/lesson2/ComparingObjects.java
@@ -1,2 +1,21 @@
-package unit3.lesson2;public class ComparingObjects {
+package unit3.lesson2;
+
+import java.util.Scanner;
+
+public class ComparingObjects {
+    public static void main(String[] args) {
+        // Every object is associated with an "id" of sorts, known as a reference.
+        // This id/reference points back to the object whenever we use it.
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+
+        if(scanner1 == scanner2) System.out.println("`scanner1` and `scanner2` are the same objects.");
+        else System.out.println("`scanner1` and `scanner2` are different objects.");
+
+        System.out.println("Assigned `scanner1` to `scanner2`.");
+        scanner1 = scanner2; // now they have the same "id"; thus, the object referred by both variables is the same.
+
+        if(scanner1 == scanner2) System.out.println("`scanner1` and `scanner2` are the same objects.");
+        else System.out.println("`scanner1` and `scanner2` are different objects.");
+    }
 }
--- src/unit3/lesson2/DeMorgansLaw.java
+++ src/unit3/lesson2/DeMorgansLaw.java
@@ -1,2 +1,40 @@
-package unit3.lesson2;public class DeMorgansLaw {
+package unit3.lesson2;
+
+import java.util.Scanner;
+
+public class DeMorgansLaw {
+    public static void main(String[] args) {
+        // DeMorgan's Law states:
+        // !(A & B) = !A | !B    (I)
+        // !(A | B) = !A & !B    (II)
+
+        // Note: Treat & as && and | as ||.
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What should the first boolean be (t/f)? ");
+        boolean bool1 = scanner.nextLine().equalsIgnoreCase("t");
+        System.out.print("What should the second boolean be (t/f)? ");
+        boolean bool2 = scanner.nextLine().equalsIgnoreCase("t");
+
+        if(!(bool1 && bool2)) {
+            System.out.println("- Case I\t\t`!(bool1 && bool2)`\t\ttrue");
+        }
+        if(!bool1 || !bool2) {
+            System.out.println("- Case I\t\t`!bool1 || !bool2`\t\ttrue");
+        }
+
+        if(!(bool1 || bool2)) {
+            System.out.println("- Case II\t\t`!(bool1 || bool2)`\t\ttrue");
+        }
+        if(!bool1 && !bool2) {
+            System.out.println("- Case II\t\t`!bool1 && !bool2`\t\ttrue");
+        }
+
+        if(!(bool1 && bool2) == (!bool1 || !bool2)) System.out.println("`!(bool1 && bool2) == (!bool1 || !bool2)`; DeMorgan's Law is true in case I!");
+        else System.out.println("DeMorgan's Law isn't a law thanks to case I being wrong :(");
+
+        if(!(bool1 || bool2) == (!bool1 && !bool2)) System.out.println("`!(bool1 || bool2) == (!bool1 && !bool2)`; DeMorgan's Law is true in case II!");
+        else System.out.println("DeMorgan's Law isn't a law thanks to case II being wrong :(");
+    }
 }
